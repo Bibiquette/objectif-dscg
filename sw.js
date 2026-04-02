916390d5e558ea0ac67267efc34157976081b2f6
Bibiquette <brieucgarnier53@gmail.com>
2026-04-02 16:26:20 +0200

    Add files via upload
    Différentes MAJ

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
 const CACHE_NAME = 'objectif-dcg-v1';
 const ASSETS = [
+  './',
   'index.html',
+  'jeu-flux.html',
+  'jeu-immo.html',
+  'jeu-bfr-tycoon.html',
+  'jeu-prov.html',
+  'jeu-anglais.html',
   'manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Installation du Service Worker et mise en cache des ressources de base
+// Installation du Service Worker et mise en cache de TOUS les jeux
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Mise en cache des ressources de révision...');
       return cache.addAll(ASSETS);
     })
   );
